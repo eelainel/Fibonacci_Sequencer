3fbd056662d5efd0ebb4ac52505f20f0e7671cae
eelainel <33838718+eelainel@users.noreply.github.com>
2018-01-02 20:32:22 -0500

    Update Fibonacci_Sequencer.py
    Corrected indentation

--- Fibonacci_Sequencer.py
+++ Fibonacci_Sequencer.py
@@ -1,25 +1,15 @@
 def fibonacci_sequencer():
-
-
-	
-	while True:
-		length=int(input("Please enter the desired length of Fibonacci sequence."))
-
-		if length>0:
-			print("Generating Fibonacci sequence now...")
-			fib_array=[0,1]
-			count=0
-			for n in range(0, length-2):
-		    		count+=1
-
-		    		fib_array.append(fib_array[count]+fib_array[count-1])
-
-		    	print (fib_array)
-		    	break
-
-
+  while True:
+    length=int(input("Please enter the desired length of Fibonacci sequence."))
+    if length>0:
+      print("Generating Fibonacci sequence now...")
+      fib_array=[0,1]
+      count=0
+      for n in range(0, length-2):
+        count+=1
+        fib_array.append(fib_array[count]+fib_array[count-1])
+        print (fib_array)
+      break
 
 #Calling the fibonacci_sequencer function
-
-
 fibonacci_sequencer()
